5121576df47320808893b5c8787f763f55e13c81
Taha Siraj <Pikachugaming899@gmail.com>
2025-07-16 07:27:56 -0700

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import CustomRoutes from './routes/CustomRoutes'
 import { useEffect } from 'react'
 import { api } from './Api'
+import { GlobalContext } from './context/Context'
 
 const App = () => {
 
-  useEffect(()=>{
+  const {state , dipatch} = useContext(GlobalContext);
+  useEffect( () => {
       const userProfile = async() => {
       try {
-        let res= await api.get("/api/v1/userprofile");
-        console.log(res.data)
+        let res= await api.get("/userprofile");
+        dipatch({type: "USER_LOGIN", user:res.data.user})
       } catch (error) {
         console.log(error)
+        dipatch({type: "USER_LOGOUT", user: null})
       }
     }
     userProfile()
