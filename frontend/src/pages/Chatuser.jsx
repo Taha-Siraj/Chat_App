5c67e3c6947fc281cbd94696ca7c3b7100daab43
Taha Siraj <Pikachugaming899@gmail.com>
2025-07-16 11:31:32 -0700

    first commit

--- frontend/src/pages/Chatuser.jsx
+++ frontend/src/pages/Chatuser.jsx
@@ -1,28 +1,71 @@
-import React from 'react'
-import Alluser from './Alluser'
-import { useParams } from 'react-router-dom'
-import { api } from '../Api'
+import React, { useContext, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { GlobalContext } from '../context/Context';
+import axios from 'axios';
+import { api } from '../Api';
+import { useEffect } from 'react';
 
-const chat = () => {
+const Chat = () => {
+  const { state } = useContext(GlobalContext);
+  const { id } = useParams(); 
+  const [message, setChatMsg] = useState("");
+  const [allmessage , setallMessage] = useState([])
 
-  const id = useParams()
-  console.log(id)
-
-  const sentMsg =  async () => {
+  const sentMsg = async () => {
     try {
-      let res = await api.post(`/chat/${id}`)
+      let res = await api.post(`/api/v1/chat/${id}`, {
+        from: id,
+        to: state.user.user_id,
+        message: message
+      });
+      fetchmsg()
+      console.log(res.data);
     } catch (error) {
+      console.log("Axios error:", error.response?.data || error.message);
+    }
+  };
+
+
+  const fetchmsg = async () => {
+    try {
+      let res = await api.get(`/api/v1/conversation/${id}`)
+      console.log(res.data)
+      setallMessage(res.data.usermsg)
       
+    } catch (error) {
+      console.log(error)
     }
   }
+useEffect(() => {
+  fetchmsg()
+} ,[] )
   return (
     <div className='py-20 bg-black h-screen text-white font-poppins'>
-      <h1 className='text-5xl font-bold' >Chat</h1>
-      <input type="text" className='py-2 px-4 rounded-md' />
-      <br />
-      <button onClick={sentMsg} className='py-2 px-4 bg-green-600 rounded-md mt-2' > Send Msg</button>
-    </div>
-  )
-}
+      <h1 className='text-5xl font-bold'>Chat</h1>
 
-export default chat
+<div>
+  {allmessage.map((msg) => (
+    <div key={msg?._id} >
+      {msg.text}
+    </div>
+  ))}
+</div>
+
+      <input
+        value={message}
+        onChange={(e) => setChatMsg(e.target.value)}
+        type="text"
+        className='text-black py-2 px-4 rounded-md'
+      />
+      <br />
+      <button
+        onClick={sentMsg}
+        className='py-2 px-4 bg-green-600 rounded-md mt-2'
+      >
+        Send Msg
+      </button>
+    </div>
+  );
+};
+
+export default Chat;
--- server.js
+++ server.js
@@ -162,25 +162,48 @@ app.get('/api/v1/allusers', async (req, res) => {
 
 app.post('/api/v1/chat/:id', async(req, res) => {
   const senderId = req.params.id;
-  const reciverId = req.body.token.user_id
+  const reciverId = req.body.token.user_id;
+   const {message} = req.body;
   try {
+    if (!reciverId || !senderId || !message) {
+      return res.status(400).send({ msg: "Missing fields" });
+    }
     let result = await msgModel.create({
       from: senderId,
       to: reciverId,
-      text: req.body.message
+      text: message
     })
-    res.status(201).send({message: "sent msg"})
+    res.status(201).send({msg: "sent msg", result})
   } catch (error) {
-    console.log(error)
-    res.status(500).send({message: "internel server error"})
+    console.error("Chat error:", error);
+    res.status(500).send({ msg: "Internal server error" });
     
   }
 })
 
+app.get("/api/v1/conversation/:id", async (req, res) => {
+  const reciverId = req.params.id;
+  const senderId = req.body.token.user_id;
 
-const _dirname = path.resolve();
-app.use('/', express.static(path.join(_dirname, '/frontend/dist')));
-app.use('/*splat', express.static(path.join(_dirname, '/frontend/dist')));
+  try {
+    let usermsg = await msgModel.find({
+      $or: [
+        { from: reciverId, to: senderId },
+        { from: senderId, to: reciverId }
+      ]
+    });
+
+    res.status(200).send({ msg: "message found", usermsg });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ msg: "internal server error" });
+  }
+});
+
+
+// const _dirname = path.resolve();
+// app.use('/', express.static(path.join(_dirname, '/frontend/dist')));
+// app.use('/*splat', express.static(path.join(_dirname, '/frontend/dist')));
 
 app.listen(5004, () => {
   console.log("Server Is running port 5004")
