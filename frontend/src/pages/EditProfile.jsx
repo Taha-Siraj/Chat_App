65962a8672a5f782ac13c8554c74f3aaf8ee1331
Taha Siraj <Pikachugaming899@gmail.com>
2025-07-17 11:03:30 -0700

    first commit

--- frontend/src/pages/EditProfile.jsx
+++ frontend/src/pages/EditProfile.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const EditProfile = () => {
   return (
-    <div className='pt-20 bg-black h-screen'>
-      <div>
-        <h1 className='text-5xl font-extrabold text-center mt-4 text-blue-800 capitalize'> edit profile </h1>
-        <div>
+    <div className='pt-20 bg-black min:h-screen'>
+      <div className='flex flex-col justify-center items-center gap-y-8'>
+        <h1 className='text-6xl font-extrabold text-center mt-4 text-[#818CF8] capitalize'> edit profile </h1>
+        <div className='px-5 flex justify-center items-center flex-col py-5 gap-y-6 rounded-xl min-h-[500px] bg-[#1F2937] w-[450px]'>
           
         </div>
       </div>
