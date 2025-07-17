e5079ecff03ade3af8f595a85fa8019a8511a9d4
Taha Siraj <Pikachugaming899@gmail.com>
2025-07-17 11:37:31 -0700

    first commit

--- frontend/src/pages/EditProfile.jsx
+++ frontend/src/pages/EditProfile.jsx
@@ -1,12 +1,53 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/Context'
+import { FaUpload } from 'react-icons/fa';
 
 const EditProfile = () => {
+  const { state } = useContext(GlobalContext);
+
+
+
+  let inputStyle = 'mt-2 bg-[#374151] outline-none focus:ring-[#6366F1] text-[15px] placeholder:capitalize focus:ring-4 py-4 px-4 rounded-lg w-full'
   return (
-    <div className='pt-20 bg-black min:h-screen'>
+    <div className='pt-20 bg-black min:h-screen font-poppins'>
       <div className='flex flex-col justify-center items-center gap-y-8'>
         <h1 className='text-6xl font-extrabold text-center mt-4 text-[#818CF8] capitalize'> edit profile </h1>
-        <div className='px-5 flex justify-center items-center flex-col py-5 gap-y-6 rounded-xl min-h-[500px] bg-[#1F2937] w-[450px]'>
-          
+        <div className='px-5 flex justify-center items-center flex-col py-6 gap-y-6 rounded-xl min-h-[500px] bg-[#1F2937] w-[500px]'>
+          <div className='flex flex-col  justify-center items-center gap-y-3'><img className='border-2 border-[#818CF8] cursor-pointer h-28 rounded-full w-28' src={state.user.profile || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSw3n-Kb2orGpTmaoHO7GOPX8_P-8-A6NO97Q&s"} alt="" />
+            <label htmlFor="photo-upload" className="cursor-pointer bg-indigo-600 text-white py-2 px-4 rounded-full text-sm font-semibold hover:bg-indigo-700 transition-colors duration-200 flex items-center gap-2">
+              <FaUpload /> Change Photo
+              <input
+               id="photo-upload"
+              type="file"
+              accept="image/*"
+              className="hidden"
+              />
+            </label>
+          </div>
+          <div className='w-full flex flex-col justify-center gap-y-4'>
+            <label className='text-sm text-white'> 
+              First Name
+              <input type="text" placeholder='first name' className={inputStyle} />
+            </label>
+            <label className='text-sm text-white'> 
+              Last Name
+              <input type="text" placeholder='first name' className={inputStyle} />
+            </label>
+            <label className='text-sm text-white'> 
+             Email Address
+              <input type="text" placeholder='your,email@gmail.com' className={inputStyle} />
+            </label>
+            <label className='text-sm text-white'> 
+             Phone Number
+              <input type="number" placeholder='eg; +92 3XXXXXXXX' className={inputStyle} />
+            </label>
+            <label className='text-sm text-white'> 
+             Bio (Optional)
+              <textarea placeholder='Tell Us about yourself...' className={inputStyle} />
+            </label>
+          </div>
+
+          <button className='py-3 px-4 w-full rounded-lg font-semibold bg-blue-700 text-white' > Save Change</button>
         </div>
       </div>
     </div>
