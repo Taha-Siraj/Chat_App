eabe6b1f619076fbe28b5bfe5ecfd0aeba18194c
Taha Siraj <Pikachugaming899@gmail.com>
2025-07-17 11:01:31 -0700

    first commit

--- frontend/src/pages/EditProfile.jsx
+++ frontend/src/pages/EditProfile.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const EditProfile = () => {
   return (
-    <div className='pt-20'>
-      <h1> edit profile </h1>
+    <div className='pt-20 bg-black h-screen'>
+      <div>
+        <h1 className='text-5xl font-extrabold text-center mt-4 text-blue-800 capitalize'> edit profile </h1>
+        <div>
+          
+        </div>
+      </div>
     </div>
   )
 }
